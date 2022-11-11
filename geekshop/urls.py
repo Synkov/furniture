7f0bad5739db95722cb3ac0d45c5ff611aa2dcd3
aleksei <synkovaleksei@yandex.ru>
2022-11-12 00:33:35 +0300

    Lesson 3

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -8,10 +8,11 @@ import mainapp.views as mainapp
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", mainapp.main, namespace="main"),
-    path("products/", include("mainapp/urls", namespace="products")),
-    path("contact/", mainapp.contact, namespace="contact"),
+    path("", mainapp.main, name="main"),
+    path("products/", include("mainapp.urls", namespace="products")),
+    path("contact/", mainapp.contact, name="contact"),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
