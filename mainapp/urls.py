d380e27e1ee9875f5add497d185ec42f763267bc
Synkov Aleksei <81877147+Synkov@users.noreply.github.com>
2022-11-12 00:34:20 +0300

    Lesson 3 (#3)
    * Lesson 3. Django ORM start
    
    * Lesson 3. Djagno admin
    
    * Lesson 3. Models in context
    
    * Lesson 3. Urls for app
    
    * Lesson 3. Code style fix
    
    * Lesson 3

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+import mainapp.views as mainapp
+
+app_name = 'mainapp'
+
 urlpatterns = [
-    path('')
+    path("", mainapp.products, name="index"),
+    path("<int:pk>/", mainapp.products, name="category"),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,64 +1,50 @@
 import datetime
+
+from django.conf import settings
 from django.shortcuts import render
+
+from mainapp.models import Product, ProductCategory
 
 
 def main(request):
-    title = 'Главная'
-    products = [
-        {
-            'name': 'Отлинчый стул',
-            'desc': 'Расположитесь комфортно',
-            'image_src': 'product-1.jpg',
-            'image_href': '/product/1/',
-            'alt': 'продукт 1'
-        },
-        {
-            'name': 'Стул повышенного качества',
-            'desc': 'Не оторваться',
-            'image_src': 'product-2.jpg',
-            'image_href': '/product/2/',
-            'alt': 'продукт 2'
-        },
-    ]
-    content = {'title': title, 'products': products}
-    return render(request, 'mainapp/index.html', content)
+    title = "Главная"
+    products = Product.objects.all()
+    content = {"title": title, "products": products, "media_url": settings.MEDIA_URL}
+    return render(request, "mainapp/index.html", content)
 
 
-def products(request):
-    title = 'продукты'
-    links_menu = [
-
-        {'href': 'products_all', 'name': 'все'},
-        {'href': 'products_home', 'name': 'дом'},
-        {'href': 'products_office', 'name': 'офис'},
-        {'href': 'products_modern', 'name': 'модерн'},
-        {'href': 'products_classic', 'name': 'классика'},
-
-    ]
-    same_products = [
-        {'name': 'Отличный стул', 'desc': 'не оторваться',
-            'image_src': 'product-11.jpg', 'alt': 'продукт 11'},
-        {'name': 'Стул повышенного качества', 'desc': 'комфортно',
-            'image_src': 'product-21.jpg', 'alt': 'продукт 21'},
-        {'name': 'Стул премиального качества', 'desc': 'Просто попробуйте',
-            'image_src': 'product-31.jpg', 'alt': 'продукт 31'},
-    ]
-    content = {'title': title, 'links_menu': links_menu,
-               'same_products': same_products}
-    return render(request, 'mainapp/products.html', content)
+def products(request, pk=None):
+    title = "продукты"
+    links_menu = ProductCategory.objects.all()
+    same_products = Product.objects.all()
+    content = {
+        "title": title,
+        "links_menu": links_menu,
+        "same_products": same_products,
+        "media_url": settings.MEDIA_URL,
+    }
+    if pk:
+        print(f"User select category: {pk}")
+    return render(request, "mainapp/products.html", content)
 
 
 def contact(request):
-    title = 'контакты'
+    title = "контакты"
     vizit_date = datetime.datetime.now()
     locations = [
-        {'city': 'Москва', 'phone': '+7-777-777-77-77',
-            'email': 'info@geekshop.ru', 'address': 'В пределах МКАД'},
-        {'city': 'Ектаеринбург', 'phone': '+8-888-888-88-88',
-            'email': 'info_yekaterinburg@geekshop.ru', 'address': 'Близко к центру'},
-        {'city': 'Владивосток', 'phone': '+9-999-999-99-99',
-            'email': 'info_vladivostok@geekshop.ru', 'address': 'Близко к океану'},
+        {"city": "Москва", "phone": "+7-777-777-77-77", "email": "info@geekshop.ru", "address": "В пределах МКАД"},
+        {
+            "city": "Ектаеринбург",
+            "phone": "+8-888-888-88-88",
+            "email": "info_yekaterinburg@geekshop.ru",
+            "address": "Близко к центру",
+        },
+        {
+            "city": "Владивосток",
+            "phone": "+9-999-999-99-99",
+            "email": "info_vladivostok@geekshop.ru",
+            "address": "Близко к океану",
+        },
     ]
-    content = {'title': title, 'vizit_date': vizit_date,
-               'locations': locations}
-    return render(request, 'mainapp/contact.html', content)
+    content = {"title": title, "vizit_date": vizit_date, "locations": locations}
+    return render(request, "mainapp/contact.html", content)
