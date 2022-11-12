a976b32378429165baa72271b702801d5341ade5
aleksei <synkovaleksei@yandex.ru>
2022-11-12 03:22:54 +0300

    Lesson 4. Code style fix

--- mainapp/models.py
+++ mainapp/models.py
@@ -13,14 +13,10 @@ class Product(models.Model):
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     name = models.CharField(verbose_name="имя продукта", max_length=128)
     image = models.ImageField(upload_to="products_images", blank=True)
-    short_desc = models.CharField(
-        verbose_name="краткое описание продукта", max_length=60, blank=True)
-    description = models.TextField(
-        verbose_name="Описание продукта", blank=True)
-    price = models.DecimalField(
-        verbose_name="цена продукта", max_digits=8, decimal_places=2, default=0)
-    quantity = models.PositiveIntegerField(
-        verbose_name="количество на складе", default=0)
+    short_desc = models.CharField(verbose_name="краткое описание продукта", max_length=60, blank=True)
+    description = models.TextField(verbose_name="Описание продукта", blank=True)
+    price = models.DecimalField(verbose_name="цена продукта", max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(verbose_name="количество на складе", default=0)
 
     def __str__(self):
         return f" {self.name}({self.category.name})"
@@ -29,8 +25,7 @@ class Product(models.Model):
 class Contact(models.Model):
     phone = models.CharField(verbose_name="номер телефона", max_length=50)
     email = models.EmailField(verbose_name="email", max_length=254)
-    city = models.CharField(verbose_name="город",
-                            max_length=128, default="Москва")
+    city = models.CharField(verbose_name="город", max_length=128, default="Москва")
     address = models.CharField(verbose_name="адрес", max_length=254)
 
     def __str__(self):
