a76fce89a7d1532ba76a5a64558e0016fd81da7b
aleksei <synkovaleksei@yandex.ru>
2022-11-14 01:04:35 +0300

    Lesson 6. Product page

--- basketapp/urls.py
+++ basketapp/urls.py
@@ -1,3 +1,4 @@
+
 from django.urls import path
 
 import basketapp.views as basketapp
@@ -10,3 +11,4 @@ urlpatterns = [
     path("", basketapp.basket, name="view"),
     path("add/<int:pk>/", basketapp.basket_add, name="add"),
     path("remove/<int:pk>/", basketapp.basket_remove, name="remove"),
+]
