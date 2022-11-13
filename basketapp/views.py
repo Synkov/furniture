e10069ef513fe8e1700e6cebb7fcbef9ee1121e2
aleksei <synkovaleksei@yandex.ru>
2022-11-13 03:15:37 +0300

    Lesson 5. Basket app start

---
+++ basketapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
+from basketapp.models import Basket
+from mainapp.models import Product
+
+
+def basket(request):
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+def basket_add(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    basket = Basket.objects.filter(user=request.user, product=product).first()
+    if not Basket:
+        basket = Basket(user=request.user, product=product)
+    basket.quantity += 1
+    basket.save()
+    return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
+
+
+def basket_remove(request):
+    return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
