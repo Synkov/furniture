a0eb958518690db586d777f38c918ae2206f0e74
aleksei <synkovaleksei@yandex.ru>
2022-11-13 23:52:19 +0300

    Lesson 6. Basket extension

--- basketapp/models.py
+++ basketapp/models.py
@@ -5,7 +5,26 @@ from mainapp.models import Product
 
 
 class Basket(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="basket")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE, related_name="basket")
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(verbose_name="количество", default=0)
-    add_datetime = models.DateTimeField(verbose_name="время добавления", auto_now_add=True)
+    quantity = models.PositiveIntegerField(
+        verbose_name="количество", default=0)
+    add_datetime = models.DateTimeField(
+        verbose_name="время добавления", auto_now_add=True)
+
+    @property
+    def product_cost(self):
+        return self.product.price * self.quantity
+
+    @property
+    def total_quantity(self):
+        _item = Basket.objects.filter(user=self.user)
+        __totalquantity = sum(list(map(lambda x: x.quantity, _items)))
+        return __totalquantity
+
+    @property
+    def total_cost(self):
+        _itams = Basket.objects.filter(user=self.user)
+        _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
+        return _totalcost
