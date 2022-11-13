fb5d6d875f79a302d4bc65a97f05e0fd3a88efd6
Synkov Aleksei <81877147+Synkov@users.noreply.github.com>
2022-11-13 03:22:40 +0300

    Lesson 5 (#5)
    * Lesson 5. ORM filters
    
    * Lesson 5. Basket app start
    
    * Lesson 5. Basket counter
    
    * Lesson 5. Code style fix

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,8 @@
 from django.conf import settings
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 
+from basketapp.models import Basket
 from mainapp.models import Contact, Product, ProductCategory
 
 
@@ -15,12 +16,33 @@ def main(request):
 def products(request, pk=None):
     title = "продукты"
     links_menu = ProductCategory.objects.all()
+
+    basket = []
+    if request.user.is_authenticated():
+        basket = Basket.objects.filter(user=request.user)
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by("price")
+            category = {"name": "все"}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by("price")
+        content = {
+            "title": title,
+            "links_menu": links_menu,
+            "category": category,
+            "products": products,
+            "media_url": settings.MEDIA_URL,
+            "basket": basket,
+        }
+        return render(request, "mainapp/products_list.html", content)
     same_products = Product.objects.all()
     content = {
         "title": title,
         "links_menu": links_menu,
         "same_products": same_products,
         "media_url": settings.MEDIA_URL,
+        "basket": basket,
     }
     if pk:
         print(f"User select category: {pk}")
