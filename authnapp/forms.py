46622c56251532030814ea2d6c66357a1968d739
aleksei <synkovaleksei@yandex.ru>
2022-11-30 02:42:05 +0300

    lesson_10. User's profile edit

--- authnapp/forms.py
+++ authnapp/forms.py
@@ -4,7 +4,7 @@ import random
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserChangeForm, UserCreationForm
 
-from .models import ShopUser
+from .models import ShopUser, ShopUserProfile
 
 
 class ShopUserLoginForm(AuthenticationForm):
@@ -35,15 +35,18 @@ class ShopUserRegisterForm(UserCreationForm):
         user = super(ShopUserRegisterForm, self).save()
 
         user.is_active = False
-        salt = hashlib.sha1(str(random.random()).encode("utf8")).hexdigest()[:6]
-        user.activation_key = hashlib.sha1((user.email + salt).encode("utf8")).hexdigest()
+        salt = hashlib.sha1(
+            str(random.random()).encode("utf8")).hexdigest()[:6]
+        user.activation_key = hashlib.sha1(
+            (user.email + salt).encode("utf8")).hexdigest()
         user.save()
 
         return user
 
     class Meta:
         model = ShopUser
-        fields = ("username", "first_name", "password1", "password2", "email", "age", "avatar")
+        fields = ("username", "first_name", "password1",
+                  "password2", "email", "age", "avatar")
 
 
 class ShopUserEditForm(UserChangeForm):
@@ -63,3 +66,14 @@ class ShopUserEditForm(UserChangeForm):
     class Meta:
         model = ShopUser
         fields = ("username", "first_name", "email", "age", "avatar")
+
+
+class ShopUserProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = ShopUserProfile
+        fields = ("tagline", "aboutMe", "gender")
+
+    def __init__(self, *args, **kwargs):
+        super(ShopUserProfileEditForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "form-control"
