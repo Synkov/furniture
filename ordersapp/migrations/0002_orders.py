165ffac60928d6878168c80ec0bdd9f9157ec254
aleksei <synkovaleksei@yandex.ru>
2022-12-13 09:27:32 +0300

    Lesson 11. Code style fix

--- ordersapp/migrations/0002_orders.py
+++ ordersapp/migrations/0002_orders.py
@@ -6,13 +6,25 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('ordersapp', '0001_orders'),
+        ("ordersapp", "0001_orders"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='order',
-            name='status',
-            field=models.CharField(choices=[('FM', 'формируется'), ('STP', 'отправлен в обработку'), ('PD', 'оплачен'), ('PRD', 'обрабатывается'), ('RDY', 'готов к выдаче'), ('CNC', 'отменен')], default='FM', max_length=3, verbose_name='статус'),
+            model_name="order",
+            name="status",
+            field=models.CharField(
+                choices=[
+                    ("FM", "формируется"),
+                    ("STP", "отправлен в обработку"),
+                    ("PD", "оплачен"),
+                    ("PRD", "обрабатывается"),
+                    ("RDY", "готов к выдаче"),
+                    ("CNC", "отменен"),
+                ],
+                default="FM",
+                max_length=3,
+                verbose_name="статус",
+            ),
         ),
     ]
